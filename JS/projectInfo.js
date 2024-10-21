46ee0d19b746aae3f0a8d5404c153b378645660b
yonghyeon11 <dydgus417@gmail.com>
2024-10-21 20:46:01 +0900

    포트폴리오 상세페이지 추가 24.10.21

---
+++ JS/projectInfo.js
@@ -0,0 +1,22 @@
+let observer =  new IntersectionObserver((e)=> {
+    e.forEach((box)=>{
+        if(box.isIntersecting){
+            box.target.style.opacity = 1;
+        }else{
+            box.target.style.opacity = 0;
+        }
+        
+    })
+})
+
+let div =  document.querySelectorAll('.as')
+
+observer.observe(div[0]) //html요소가 화면에 등장하는지 감시
+observer.observe(div[1])
+observer.observe(div[2])
+observer.observe(div[3])
+observer.observe(div[4])
+observer.observe(div[5])
+observer.observe(div[6])
+observer.observe(div[7])
+observer.observe(div[8])
